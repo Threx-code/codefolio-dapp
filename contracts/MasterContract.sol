09a2ddbe9dab0f05ee61788bef08baf614ce381f
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-01-31 11:26:01 +0100

    Adding the pool updating functions

--- contracts/MasterContract.sol
+++ contracts/MasterContract.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "./utils/ReentrancyGuard.sol";
 import {CodefolioRewards} from "./CodefolioRewards.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract MasterContract is Ownable, ReentrancyGuard
 {
@@ -35,6 +36,9 @@ contract MasterContract is Ownable, ReentrancyGuard
     uint256 public startBlock;
     uint256 public BONUS_MULTIPLIER;
 
+
+    event PoolAdded(address indexed lpToken, uint256 allocPoint, uint256 lastRewardBlock);
+
     constructor(
         CodefolioRewards _cdr,
         address _devAddr,
@@ -62,10 +66,31 @@ contract MasterContract is Ownable, ReentrancyGuard
     }
 
 
+     modifier validatePool(uint256 _pid){
+        require(_pid < poolInfo.length, "Pool does not exist");
+        _;
+    }
+
+
+    /**
+     * @notice Retrieves the total number of pools.
+     * @dev Returns the length of the `poolInfo` array, representing the total number of liquidity pools.
+     * @return uint256 The total number of pools.
+     */
     function poolLength() external view returns (uint256){
         return poolInfo.length;
     }
 
+    /**
+     * @notice Retrieves information about a specific pool.
+     * @dev Returns key details of the specified pool, including the LP token address,
+     *      allocation points, last reward distribution block, and reward per share.
+     * @param _pid The ID of the pool.
+     * @return lpToken The address of the LP token associated with the pool.
+     * @return allocPoint The allocation points assigned to the pool.
+     * @return lastRewardBlock The last block number at which rewards were distributed.
+     * @return rewardTokenPerShare The accumulated reward tokens per share for the pool.
+     */
     function getPoolInfo(uint256 _pid) public view returns (
         address lpToken,
         uint256 allocPoint,
@@ -82,15 +107,40 @@ contract MasterContract is Ownable, ReentrancyGuard
         );
     }
 
+
+    /**
+     * @notice Calculates the multiplier for a given range of pools.
+     * @dev The multiplier is computed by multiplying the difference between the 
+     *      two pool numbers (_from and _to) by the BONUS_MULTIPLIER constant.
+     *      This function helps in determining the reward multiplier for a specified pool range.
+     * @param _from The starting pool number.
+     * @param _to The ending pool number.
+     * @return uint256 The calculated multiplier for the specified pool range.
+     */
     function getMultiplier(uint256 _from, uint256 _to) public view returns(uint256){
         return (_to - _from) * BONUS_MULTIPLIER;
     }
 
 
+    /**
+     * @notice Updates the reward bonus multiplier.
+     * @dev This function allows the contract owner to update the bonus multiplier, 
+     *      which affects the reward calculations. The function can only be called by the owner.
+     * @param _multiplier The new multiplier value to be set.
+     * @dev The new multiplier will be used in reward calculations across the contract.
+     */
     function updateMultiplier(uint256 _multiplier) public onlyOwner{
         BONUS_MULTIPLIER = _multiplier;
     }
 
+    /**
+     * @notice Checks if a pool with the given Liquidity Pool (LP) token already exists.
+     * @param _lpToken The address of the Liquidity Pool token to check for duplication.
+     * @dev This function iterates through all existing pools and verifies that the provided LP token
+     *      is not already associated with an existing pool. If a duplicate pool is found, an error is thrown.
+     *      The function uses a `require` statement to enforce this check.
+     * @custom:throw Error "Pool already exists" if a pool with the same LP token is found.
+     */
     function checkDuplicatePool(IERC20 _lpToken) public view {
         uint256 lengthOfPool = poolInfo.length;
         for(uint256 _pid = 0; _pid < lengthOfPool; _pid++){
@@ -98,7 +148,13 @@ contract MasterContract is Ownable, ReentrancyGuard
         }
     }
 
-
+    /**
+     * @notice Updates the staking pool's allocation points.
+     * @dev Recalculates the allocation points for the staking pool.
+     * @dev The first pool (index 0) is always the staking pool.
+     *      The new allocation points are set as one-third of the total allocation points of other pools.
+     *      Ensures dynamic adjustment of staking rewards based on other pools' allocations.
+     */
     function updateStakingPool() internal {
         uint256 lengthOfPool = poolInfo.length;
         uint256 points = 0;
@@ -116,6 +172,19 @@ contract MasterContract is Ownable, ReentrancyGuard
     }
 
 
+    /**
+     * @notice Adds a new pool to the contract
+     * @param _allocPoint The allocation points for the new pool
+     * @param _lpToken The address of the Liquidity Pool token associated with the new pool
+     * @dev This function performs several tasks:
+     *      - First, it checks if the pool already exists using the `checkDuplicatePool` function to prevent duplicates.
+     *      - Then, it calculates the last reward block (either the current block or the start block, whichever is greater).
+     *      - The function increases the `totalAllocation` by the provided allocation points.
+     *      - A new pool is added to the `poolInfo` array, initializing the `lpToken`, `allocPoint`, `lastRewardBlock`, and `rewardTokenPerShare` values.
+     *      - Finally, it calls `updateStakingPool` to update the staking pool allocation.
+     * @custom:throw Error "Pool already exists" if the pool with the same LP token is found.
+     * @custom:emit Emits an event to log the addition of a new pool (if events are included in the contract).
+     */
     function addPool(uint256 _allocPoint, IERC20 _lpToken) public onlyOwner {
         checkDuplicatePool(_lpToken);
 
@@ -133,6 +202,37 @@ contract MasterContract is Ownable, ReentrancyGuard
 
         updateStakingPool();
 
+        emit PoolAdded(address(_lpToken), _allocPoint, lastRewardBlock);
+    }
+
+
+   
+
+    function updatePool(uint256 _pid) public validatePool(_pid) onlyOwner{
+        PoolInfo storage pool = poolInfo[_pid];
+        if(block.number < pool.lastRewardBlock){
+            return;
+        }
+
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if(lpSupply == 0){
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 tokenReward = (multiplier * cdrPerBlock * pool.allocPoint) / totalAllocation;
+
+        cdr.mint(devAddr, tokenReward / 10);
+        cdr.mint(address(this), tokenReward);
+
+
+        // use factor of twelve Szabo/Microether/Micro
+        pool.rewardTokenPerShare = pool.rewardTokenPerShare + (tokenReward * 1e12) / lpSupply;
+
+        pool.lastRewardBlock = block.number;
+
+    
     }
 
 
